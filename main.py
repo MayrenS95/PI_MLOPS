9c7fbff7648d71640bf7d4b9a2dadfc8cb83838a
unknown <mayrensilva95@gmail.com>
2023-08-08 21:23:28 -0500

    modificaciones de forma

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ import pandas as pd
 import numpy as np
 import ast
 
+#definiendo el acceso al repositorio de GitHub
 #url_csv = 'https://raw.githubusercontent.com/MayrenS95/PI_MLOPS/main/df_final_movies_.csv'
 url_csv = 'https://raw.githubusercontent.com/MayrenS95/PI_MLOPS/main/df_final_movies_.csv'
 df_movies_final = pd.read_csv(url_csv)
 
+#instanciando la función
 app = FastAPI()
 
-
+#desarrollando la función de consulta idiomas
 @app.get('/peliculas_idioma/{idioma}')
 def peliculas_idioma(idioma:str):
     '''Ingresas el idioma, retornando la cantidad de peliculas producidas en el mismo'''
 
+#filtrando los valores del df a través de una mascara
     respuesta = df_movies_final['original_language'][df_movies_final['original_language']==idioma]
 
     return {'idioma':idioma, 'cantidad':respuesta.shape[0]}
 
 
-    
+#Definiendo la funciòn de pelicula
 @app.get('/peliculas_duracion/{pelicula}')
 def peliculas_duracion(pelicula:str):
     '''Ingresas la pelicula, retornando la duracion y el año'''
@@ -32,6 +35,7 @@ def peliculas_duracion(pelicula:str):
     return {'pelicula':pelicula, 'duracion':respuesta, 'anio':anio}
 
 
+#definiendo la función consulta franquicia
 
 @app.get('/franquicia/{franquicia}')
 def franquicia(franquicia:str):
@@ -46,7 +50,7 @@ def franquicia(franquicia:str):
             }
 
 
-
+#definiendo la función de pais de producción
 @app.get('/peliculas_pais/{pais}')
 def peliculas_pais(pais:str):
     '''Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo'''
@@ -61,7 +65,7 @@ def peliculas_pais(pais:str):
     return {'pais':pais, 'cantidad':respuesta}
 
 
-
+#definiendo la funcion de compañias productoras exitosas, con el revenue
 @app.get('/productoras_exitosas/{productora}')
 def productoras_exitosas(productora:str):
     '''Ingresas la productora, entregandote el revenue total y la cantidad de peliculas que realizo '''
@@ -78,7 +82,7 @@ def productoras_exitosas(productora:str):
 
     return {'productora':productora, 'revenue_total': revenue_total, 'cantidad':cantidad}
 
-
+#definiendo la funcion de director
 @app.get('/get_director/{nombre_director}')
 def get_director(nombre_director:str):
     ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
@@ -132,21 +136,38 @@ def get_director(nombre_director:str):
     'budget_pelicula':lista_budget, 'revenue_pelicula':lista_revenue
             }
         
+
+#ML --- Sistema de recomendación
 @app.get('/recomendacion/{titulo}')
 def recomendacion(titulo:str):
     '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+    #se define una cantidad especifica a usar del dataset por razones de rendimiento del equipo
     df = df_movies_final.loc[0:4000]
+
+    #se importan librerias para usar la función tf - idf vector
     from sklearn.feature_extraction.text import TfidfVectorizer
+
+    #se instancia la función, pasando las palabras a omitir en una lista 
     tfidf = TfidfVectorizer(stop_words='english')
+
+    #se sustituyen los nulos en la columna a usar para el sistema de recomendación
     df['genres'] = df['genres'].fillna('')
+
+    #se trasnforman los datos de la columna a usar en matriz
     tfidf_matrix = tfidf.fit_transform(df['genres'])
+
+    #se importa la libreria para usar la funcion del coseno
     from sklearn.metrics.pairwise import linear_kernel
 
+    #se calcula el coseno con la multiplicación de las matrices
     cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    #se establece los indices de la etiqueta a generar
     indices = pd.Series(df.index, index=df['title']).drop_duplicates()
 
-
+    #se establece la función que llevará a cabo los cálculos automaticamente
     def recomendation(title, cosine_sim=cosine_sim):
+        
         idx = indices[title]
 
         sim_scores = list(enumerate(cosine_sim[idx]))
